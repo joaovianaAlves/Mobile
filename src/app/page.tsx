d38adb4fef79032776f7c322e1239e16226a4a19
João Vitor <joaovitorvianaalves@gmail.com>
2024-03-11 18:14:49 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,10 @@
 "use client";
 import NavBar from "@/components/NavBar";
-import { useState } from "react";
 
 export default function Home() {
-  const [isTextEnabled, setIsTextEnabled] = useState(false);
   return (
     <div className="h-screen flex flex-col">
-      <NavBar />
-      <main className="flex-1 flex items-center justify-center">
-        {isTextEnabled ? (
-          <p>Hello World Ads</p>
-        ) : (
-          <button
-            className="font-bold p-4 rounded-lg bg-slate-200"
-            onClick={() => setIsTextEnabled(true)}
-          >
-            Clique Aqui
-          </button>
-        )}
-      </main>
+      <main className="flex-1 flex items-center justify-center"></main>
     </div>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,27 @@
-import Image from "../../node_modules/next/image";
-import { LiaHomeSolid } from "react-icons/lia";
-import { CiSquarePlus } from "react-icons/ci";
+import { AiFillHome } from "react-icons/ai";
+import { IoMdPerson } from "react-icons/io";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 export default function NavBar() {
   return (
-    <header className="flex gap-2 justify-between items-center p-2 bg-slate-100 px-6">
-      <div className="flex gap-4 items-center">
-        <Image src={"/Logo.svg"} alt="" width={30} height={30} />
-        <h1>Lorem ipsum</h1>
-      </div>
-      <nav>
-        <ul className="flex gap-3 items-center justify-center">
-          <li>
-            <a href="" className="font-bold">
-              <LiaHomeSolid size={25} />
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <nav className="flex gap-2 justify-between items-center p-5 bg-gray-300 px-4">
+      <ul className="flex w-full items-center justify-evenly">
+        <li>
+          <a href="" className="font-bold" title="Previous">
+            <AiFillHome size={25} aria-label="Home" />
+          </a>
+        </li>
+        <li>
+          <a href="" className="font-bold" title="Home">
+            <FaMagnifyingGlass size={25} aria-label="Search" />
+          </a>
+        </li>
+        <li>
+          <a href="" className="font-bold" title="Search">
+            <IoMdPerson size={25} aria-label="Home" />
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
