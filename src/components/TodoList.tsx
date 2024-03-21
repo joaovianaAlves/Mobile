2d6344ba6427db428eb81bd4eee9f88cd637d191
joaovianaAlves <joaovitorvianaalves@gmail.com>
2024-03-20 21:30:19 -0300

    .

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import Search from "./Search";
 import { FaRegTrashCan } from "react-icons/fa6";
 import { FaCheck } from "react-icons/fa";
